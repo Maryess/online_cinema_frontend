4abcdd73586e15b974b0acb6bf4537afaf6c036b
Maryess <vip.ivan.ivanov2003@bk.ru>
2024-10-23 19:42:54 +0300

    refactoring code and change styles with functional components

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,4 @@
-import Button from '@/components/button/button';
+import Button from '@/components/button/Button';
 import Field from '@/components/field/Field';
 import styles from './auth.module.css';
 
@@ -9,20 +9,18 @@ export default function AuthPage() {
 				<div className={styles.element_top}></div>
 				<div className={styles.element_bottom}></div>
 			</div>
-			<div className={styles.auth}>
-				<form className={styles.auth_form} action=''>
-					<h2 className={styles.title}>Log in</h2>
-					<div className={styles.fields}>
-						<Field type='email' placeholder='Email' />
-						<Field type='password' placeholder='Password' />
-						<Field type='text' placeholder='Your name' />
-					</div>
-					<div>
-						<Button text='Log in' />
-						<Button text='Sign in' />
-					</div>
-				</form>
-			</div>
+			<form className={styles.auth} action=''>
+				<h2 className={styles.title}>Log in</h2>
+				<div className={styles.fields}>
+					<Field type='email' placeholder='Email' />
+					<Field type='password' placeholder='Password' />
+					<Field type='text' placeholder='Your name' />
+				</div>
+				<div>
+					<Button text='Sign in' />
+					<Button text=' Sign up' />
+				</div>
+			</form>
 			<div className={styles.right_element}>
 				<div className={styles.element_top}></div>
 				<div className={styles.element_bottom}></div>
--- components/hamburger/hamburger.tsx
+++ components/hamburger/hamburger.tsx
@@ -1,25 +1,6 @@
-'use client';
-
-import { useState } from 'react';
 import { BsFillMenuButtonWideFill } from 'react-icons/bs';
+import styles from './Hamburger.module.css';
 
 export default function Hamburger() {
-	const [show, setShow] = useState(false);
-
-	function showMenu() {
-		setShow(prev => !prev);
-	}
-
-	return (
-		<div>
-			<BsFillMenuButtonWideFill
-				onClick={showMenu}
-				width={60}
-				height={60}
-				className='bg-gray-600 '
-			/>
-
-			{show === true ? <p className='text-white'>show</p> : <p>onShow</p>}
-		</div>
-	);
+	return <BsFillMenuButtonWideFill className={styles.hamburger} />;
 }
