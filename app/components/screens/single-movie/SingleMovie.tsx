91ba6ef62b90ac2d7665d05f9a0144079d08b33a
Mary <vip.ivan.ivanov2003@bk.ru>
2025-05-24 00:17:33 +0300

    changed styles

--- app/components/screens/single-movie/SingleMovie.tsx
+++ app/components/screens/single-movie/SingleMovie.tsx
@@ -1,30 +1,41 @@
-import { FC } from 'react'
-import Meta from 'utils/meta/Meta'
-import { IMoviePage } from '../../../../pages/movie/[slug]'
-import dynamic from 'next/dynamic'
-import Banner from 'components/ui/banner/Banner'
-import Description from 'components/ui/banner/Info/Description'
+import { FC } from "react";
+import Meta from "utils/meta/Meta";
+import { IMoviePage } from "../../../../pages/movie/[slug]";
+import dynamic from "next/dynamic";
+import Banner from "components/ui/banner/Banner";
+import Description from "components/ui/banner/Info/Description";
+import { SERVER_URL } from "config/api.config";
 
-const DynamicPlayer = dynamic(()=> import('components/ui/video-player/VideoPlayer'), {
-    ssr:false
-})
+const DynamicPlayer = dynamic(
+	() => import("components/ui/video-player/VideoPlayer"),
+	{
+		ssr: false,
+	}
+);
 
-const SingleMovie:FC<IMoviePage> = ({movie}) => {
+const SingleMovie: FC<IMoviePage> = ({ movie }) => {
+	return (
+		<Meta title={movie.slug} description="">
+			<Banner
+				type="movie"
+				imagePath={movie.bigPoster}
+				info={{
+					title: movie.name,
+					description: `${movie.year}, ${movie.country}, ${movie.duration} min`,
+					genres: movie.genres,
+					actors: movie.actors,
+				}}
+			/>
+			<DynamicPlayer
+				subtitleSource={`/uploads/movies/${movie.slug}.vtt`}
+				videoSource={movie.videoUrl}
+				slug={movie.slug}
+			/>
 
-    
-  return (
-    <Meta title={movie.slug} description=''>
-        <Banner type='movie' imagePath={movie.bigPoster} info={{
-          title:movie.name,
-          description:`${movie.year}, ${ movie.country}, ${movie.duration} min`,
-          genres:movie.genres,
-          actors:movie.actors
-        }}/>
-        <DynamicPlayer videoSource={movie.videoUrl} slug={movie.slug}/>
-        
-        <Description description={movie.deskription}/>
-    </Meta>
-  )
-}
+			<Description description={movie.deskription} />
+		</Meta>
+	);
+};
 
-export default SingleMovie
+export default SingleMovie;
+
